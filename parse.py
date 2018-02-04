f3fd7389540f0b0ebb809c73d3014b4bebb67e8a
daming_home <lda_ming@163.com>
2018-02-04 14:10:20 +0800

    update fix hw file

--- parse.py
+++ parse.py
@@ -3,11 +3,15 @@ import os
 import gzip
 import tarfile
 
+import logging
+
+logging.basicConfig(format= '%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.ERROR,filename="./test.log",filemode="a")
+#logging.error("testloging")
 #read needed counter in conf file
 conf = open("pm.conf","rb")
 list = [line.strip() for line in conf]
 conf.close()
-print list
+#print list
 
 # unzip .gz file
 def un_gz(filename):
@@ -27,11 +31,53 @@ def un_tar_gz(filename):
         tar.extract(name,filename + "_files/")
     tar.close()
 
+def pasre_pm(Measurements,list):
+    PmName = Measurements.find('PmName')
+
+    list1 = []
+    for pmcounter in list:
+        for child in PmName:
+            if(pmcounter == child.text):
+                list1.append(child.attrib)
+
+    record = open("record.csv","a+")
+    PmData = Measurements.find('PmData')
+    for Pm in PmData:
+        count = 0
+        cell_name = Pm.get('Dn')
+        record.write(str(cell_name)+",")
+        for key in list1:
+            value = ''
+            for child in Pm:
+                if(child.attrib == key):
+                    value = child.text
+                    break;
+            count += 1
+            if (count != len(list1)):
+                try:
+                    record.write(str(value)+",")
+                except e:
+                    logging.error(str(e))
+            else:
+                record.write(str(value))
+        record.write('\n')
+
+    record.close()
+
+
 # parse pm xml
-tree = ET.parse("test_pm.xml")
+tree = ET.parse("ENB-PM-V2.8.1-EutranCellTdd-20171130-1515P00.xml")
 root = tree.getroot()
 header = root.find('FileHeader')
 vertor = header.find('VendorName').text
+for child in root:
+    if(child.name == "Measurements"):
+        pasre_pm(child,list)
+
+
+
+
+
 Measurements = root.find('Measurements')
 PmName = Measurements.find('PmName')
 
@@ -41,20 +87,26 @@ for pmcounter in list:
         if(pmcounter == child.text):
            list1.append(child.attrib)
 
-print list1
+#print list1
 
 record = open("record.csv","w")
 PmData = Measurements.find('PmData')
 for Pm in PmData:
     count = 0
+    cell_name = Pm.get('Dn')
+    record.write(str(cell_name)+",")
     for key in list1:
+        value = ''
         for child in Pm:
             if(child.attrib == key):
                 value = child.text
-                count += 1
                 break;
+        count += 1
         if (count != len(list1)):
-            record.write(str(value)+",")
+            try:
+                record.write(str(value)+",")
+            except e:
+                logging.error(str(e))
         else:
             record.write(str(value))
     record.write('\n')
